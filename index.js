0f969fcae55db46bd19836d56a9989f0a746a0d0
Sakthiganesan91 <sakthiganesan158@gmail.com>
2024-09-10 17:36:12 +0530

    first publish

--- index.js
+++ index.js
@@ -1,32 +1,44 @@
 const initializeDateObject = () => new Date();
 
-const getdayMonthYear = (
-  date,
-  locales = "en-US",
-  options = { month: "long" }
-) => {
+const getdayMonthYear = (date, locales, options) => {
   const dateOfMonth = date.getDate();
-  const month = new Intl.DateTimeFormat(locales, options).format(date);
-  const year = date.getFullYear();
+  const day = new Intl.DateTimeFormat(locales, {
+    weekday: options.weekday,
+  }).format(date);
 
-  return { date: dateOfMonth, month, year };
+  const month = new Intl.DateTimeFormat(locales, {
+    month: options.month,
+  }).format(date);
+  const year = new Intl.DateTimeFormat(locales, {
+    year: options.year,
+  }).format(date);
+
+  return { date: dateOfMonth, month, year, day };
 };
 
-const getDate = (locales = "en-US", options = {}) => {
+const getDate = ({
+  locales = "en-US",
+  monthFormat = "long",
+  weekDayFormat = "long",
+  yearFormat = "numeric",
+} = {}) => {
   try {
+    const options = {
+      month: monthFormat,
+      weekday: weekDayFormat,
+      year: yearFormat,
+    };
     let dateObject = initializeDateObject();
 
-    const { date, month, year } = getdayMonthYear(dateObject, locales, options);
-    return { date, month, year };
+    const { date, month, year, day } = getdayMonthYear(
+      dateObject,
+      locales,
+      options
+    );
+    return { date, month, year, day };
   } catch (error) {
     throw new Error(`Error : ${error.message}`);
   }
 };
 
-console.log(
-  getDate("en-IN", {
-    month: "long",
-  })
-);
-
-module.exports = { getDate };
+export default getDate;
